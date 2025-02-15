eda9b9fb6a0cc53ed888f3505211b64b5de06d84
Aryanpatel2001 <patelaryan8044@gmail.com>
2025-02-15 11:52:46 +0530

    modify build/utils

--- build/utils/jwt.js
+++ build/utils/jwt.js
@@ -7,16 +7,18 @@ const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || "300", 10)
 const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || "1200", 10);
 //options for cookies
 exports.accessTokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpire * 3600 * 1000),
-    maxAge: accessTokenExpire * 3600 * 1000,
+    expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+    maxAge: accessTokenExpire * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
+    secure: true,
 };
 exports.refreshTokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire * 24 * 3600 * 1000),
-    maxAge: refreshTokenExpire * 24 * 3600 * 1000,
+    expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
+    secure: true,
 };
 const sendToken = (user, statusCode, res) => {
     const accessToken = user.SignAccessToken();
@@ -25,9 +27,9 @@ const sendToken = (user, statusCode, res) => {
     //upload session to redis
     redis_1.redis.set(user._id, JSON.stringify(user));
     //only set secure to true in production
-    if (process.env.NODE_ENV === "production") {
-        exports.accessTokenOptions.secure = true;
-    }
+    // if (process.env.NODE_ENV === "production") {
+    //   accessTokenOptions.secure = true;
+    // }
     res.cookie("access_token", accessToken, exports.accessTokenOptions);
     res.cookie("refresh_token", refreshToken, exports.refreshTokenOptions);
     res.status(statusCode).json({
